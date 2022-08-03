28ec1d68b2d4c6c76b490cb070205abe3a4ae867
MarkGoren <gormark2001@gmail.com>
2022-08-03 10:12:56 +0300

    clients and number generator

--- src/publishers/numbers_generator.js
+++ src/publishers/numbers_generator.js
@@ -1,11 +1,28 @@
-class NumGenerator {
+import { EventEmitter } from "events"
+
+class NumGenerator extends EventEmitter{
     constructor(limit = 100){
-        this.generator = this.printNumbers(limit)
+        this.generator = this.generateNumbers(limit)
     }
 
-    *printNumbers(limit){
-        for (let i=0; i<= limit; i++){
+    *generateNumbers(limit){
+        for (let i=1; i<= limit; i++){
             yield i
         }
     }
+
+    printNumbers(){
+        const myInterval = setInterval(()=>{
+            
+            var value = this.generator.next().value
+            if(!value){
+                clearInterval(myInterval)
+                return
+            }
+            console.log(value)
+            this.emit('numberRelease', value)
+        }, 1000)
+    }
 }
+
+export default NumGenerator
